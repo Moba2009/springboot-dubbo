5d7e307c6e2c696dc0e3635fdd4169b9baf27cce
Mojo <1316578052@qq.com>
2020-04-15 09:53:27 +0800

    1

---
+++ shop-api-common/src/main/java/com/example/spring/dubbo/App.java
@@ -0,0 +1,20 @@
+package com.example.spring.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName app
+ * @Description TODO
+ * @Author Mojo
+ * @Date 2020/4/15 9:51
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
